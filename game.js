234a94102053ee46ea4c2e614c3702e6cda61b7a
Aquiles <Aquileshr92@gmail.com>
2024-07-18 16:42:13 +0200

    Functional Game Ready

--- game.js
+++ game.js
@@ -14,10 +14,35 @@ const controlsButton = document.querySelector(".controls-button");
 const controlsSection = document.querySelector("#controls-section");
 const creditsButton = document.querySelector(".credits-button")
 const creditSection = document.querySelector("#credit-section")
+const backButton = document.querySelector(".back-button")
+
+let mainTheme = new Audio ("Assets/Sound/Test Subject Shadows_U.731.wav")
+
+
+startGameButton.addEventListener("mouseover",() => {
+  let hoverSound = new Audio ("Assets/Sound/ hoover sound.mp3")
+  hoverSound.play()
+})
+
+controlsButton.addEventListener("mouseover",() => {
+  let hoverSound = new Audio ("Assets/Sound/ hoover sound.mp3")
+  hoverSound.play()
+})
+
+creditsButton.addEventListener("mouseover",() => {
+  let hoverSound = new Audio ("Assets/Sound/ hoover sound.mp3")
+  hoverSound.play()
+})
+
+
+backButton.addEventListener("click", () => {
+  creditSection.style.display = "none";
+  /* startGameElement.style.display = "flex" */
+})
 
 creditsButton.addEventListener("click", () => {
   creditSection.style.display = "flex";
-  let mainTheme = new Audio ("Assets/Sound/Test Subject Shadows_U.731.wav")
+  /* let mainTheme = new Audio ("Assets/Sound/Test Subject Shadows_U.731.wav") */
   mainTheme.play()
 });
 
@@ -36,6 +61,7 @@ startGameButton.addEventListener("click", () => {
     startGame();
     let startButton = new Audio ("Assets/Sound/ESM_GFTL_fx_transition_one_shots_glass_movement_energy_magic_short.wav")
     startButton.play()
+    mainTheme.pause()
   }
 });
 
@@ -76,6 +102,7 @@ function startGame() {
   let backgroundSound = new Audio('Assets/Sound/ESM_Horror_Game_Ambience_Loop_Deadly_Fog_Full_Cemetary_Wind_Howl_Creepy_Scary_Soundscape.wav');
   backgroundSound.loop = true; // Enable looping
   backgroundSound.play();
+  backgroundSound.volume = 0.6;
 
   for (let i = 0; i < numberOfNurses; i++) {
     const nurse = new Nurse(nurseVelocity);
@@ -92,8 +119,8 @@ const doorArray = [];
 class Door {
   constructor() {
     this.createDoorElement();
-    this.positionX = gameArea.offsetWidth - this.element.offsetWidth - 10;
-    this.positionY = gameArea.offsetHeight - this.element.offsetHeight - 40;
+    this.positionX = Math.floor(Math.random() * (gameArea.offsetWidth - this.element.offsetWidth )); // Random X position
+    this.positionY = Math.floor(Math.random() * (gameArea.offsetHeight - this.element.offsetHeight))
     this.updateElementPosition();
     doorArray.push(this);
   }
